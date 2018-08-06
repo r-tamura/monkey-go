8edccccf364f9525b6cda21628affed5bd2db22a
r-tamura <rtamura30@gmail.com>
2018-08-06 22:33:29 +0900

    Add The first lexter test

---
+++ lexer/lexer_test.go
@@ -0,0 +1,31 @@
+package lexer
+
+import (
+	"testing"
+
+	"monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+{},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+	}
+
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			// %qは空白などをエスケープシーケンスに置き換えシングルクオートで囲んだ文字を表示する
+			t.Fatalf("text[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("text[%d] - literal wrong. expected=%q, got%q")
+		}
+	}
+}
