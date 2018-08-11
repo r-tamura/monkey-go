fa23180837ecafe1945797416e9fb8600de2136e
r-tamura <rtamura30@gmail.com>
2018-08-11 20:44:45 +0900

    Add Chapter 2.5 String method to Node

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,15 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 // Node 全てのASTの要素はNodeである
 type Node interface {
 	// デバッグテスト用
 	TokenLiteral() string
+	String() string
 }
 
 // Statement ステートメントNode
@@ -36,7 +40,15 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-// LetStatement let句
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+// LetStatement let文
 type LetStatement struct {
 	Token token.Token // the token.LET token
 	Name  *Identifier
@@ -48,13 +60,63 @@ func (ls *LetStatement) statementNode() {}
 // TokenLiteral Nodeリテラル実装
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ReturnStatement return文
 type ReturnStatement struct {
 	Token       token.Token // the token.RETURN token
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) statementNode() {}
+
+// TokenLiteral Nodeリテラル実装
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+// ExpressionStatement 式
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) expressionNode() {}
+
+// TokenLiteral Nodeリテラル実装
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
 
 // Identifier 識別子
 // let句ではIdentifierは値を返さないが、Monkey内で位置によっては値を生成する場合があるのでExpressionを実装(P45)
@@ -67,3 +129,5 @@ func (i *Identifier) expressionNode() {}
 
 // TokenLiteral Nodeリテラル実装
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (i *Identifier) String() string { return i.Value }
