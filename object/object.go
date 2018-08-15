2ed12c8cd907893af0cf1a482f078c3c69c0f9ab
r-tamura <rtamura30@gmail.com>
2018-08-15 12:07:18 +0900

    Add Chapter 3.4 Integer Boolean Null

---
+++ object/object.go
@@ -0,0 +1,50 @@
+package object
+
+import "fmt"
+
+// ObjectType token.Tokenと同じような型
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+// Object 全ての値は異なる型で定義される
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+// Integer integer型
+type Integer struct {
+	Value int64
+}
+
+// Type fullfil the object.Object interface
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+// Inspect fullfil the object.Object interface
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+// Boolean boolean型
+type Boolean struct {
+	Value bool
+}
+
+// Type fullfil the object.Object interface
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+// Inspect fullfil the object.Object interface
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+
+// Null null型 nullを実装することでnullの実装について学ぶ
+type Null struct {
+}
+
+// Type fullfil the object.Object interface
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+
+// Inspect fullfil the object.Object interface
+func (n *Null) Inspect() string { return "null" }
