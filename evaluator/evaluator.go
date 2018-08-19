bfb33a828e235865877d281cd132763950bedd79
r-tamura <rtamura30@gmail.com>
2018-08-19 22:35:26 +0900

    Add Chapter 4.6 Buildin puts

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -101,6 +101,14 @@ var builtins = map[string]*object.Builtin{
 			return &object.Array{Elements: newElements}
 		},
 	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
+		},
+	},
 }
 var (
 	// NULL Null用オブジェクト
